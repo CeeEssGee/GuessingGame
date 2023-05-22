46c38c8067739dcc6da24558b593816ac908b065
Courtney Gulledge <courtney.gulledge@gmail.com>
2023-05-22 12:55:40 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+// Display a message to the user asking them to guess the secret number.
+Console.WriteLine("Guess the secret number.");
+
+// Display a prompt for the user's guess.
+int input = Console.ReadLine();
+
+// Take the user's guess as input and save it as a variable.
+// Display the user's guess back to the screen.
